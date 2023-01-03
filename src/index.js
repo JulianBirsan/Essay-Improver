e1de1d8cb985540c3b3cee1d7fb4437b0ff8d2dc
JulianBirsan <73716417+JulianBirsan@users.noreply.github.com>
2023-01-03 14:28:13 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function MyForm() {
+  const [essay, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(essay);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your essay:
+        <input 
+          type="text" 
+          value={essay}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<MyForm />);
